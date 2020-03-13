33cf9b616cc83cc32a2399aa76c440750a06f224
wty715 <wty715@163.com>
2020-03-13 16:53:49 +0800

    Update BNF

---
+++ DataType.h
@@ -0,0 +1,9 @@
+#ifndef _DATATYPE_H
+#define _DATATYPE_H
+
+#include <string>
+enum dataType_t {INT, FLOAT, STRING, DATE};
+std::string typeName[] = {"INT", "FLOAT", "STRING", "DATE"};
+
+
+#endif _DATATYPE_H
